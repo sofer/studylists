f50c19e508e37ced4c1b110e11a186c72ad6d7ae
D Sofer <d@sofer.com>
2010-07-19 17:53:51 +0100

    respond to json requests in subjects controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,2 +1,11 @@
 class SubjectsController < InheritedResources::Base
+
+  def index
+    @subjects = Subject.all
+    respond_to do |format|
+      format.html
+      format.json  { render :json => @subjects }
+    end
+  end
+
 end
