1716e50bbd3640e1970e0f57fdce4734a5ece6fa
Danny Sofer <danny@Works-iMac.local>
2010-08-31 13:11:06 +0100

    update to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,7 @@
 Subject.delete_all
 
 subject = Subject.create :name => 'French'
-
 numbers_topic = subject.topics.create :name => 'Numbers', :case_sensitive => false, :ignore_punctuation => true
-
 number_exercises = [
   { 
     :phrase => 'un',
@@ -54,9 +52,58 @@ number_exercises = [
     :response => 'ten'
   }
 ]
-
 number_exercises.each do |ex|
   numbers_topic.exercises.create :phrase => ex[:phrase], :response => ex[:response]
 end
-
 puts "added #{numbers_topic.name} with exercises to #{subject.name}" 
+
+people_topic = subject.topics.create :name => 'People', :case_sensitive => false, :ignore_punctuation => true
+people_exercises = [
+  { 
+    :phrase => 'homme',
+    :response => 'man'
+  },
+  { 
+    :phrase => 'femme',
+    :response => 'woman'
+  },
+  { 
+    :phrase => 'garçon',
+    :response => 'boy'
+  },
+  { 
+    :phrase => 'fille',
+    :response => 'girl'
+  }
+]
+people_exercises.each do |ex|
+  people_topic.exercises.create :phrase => ex[:phrase], :response => ex[:response]
+end
+puts "added #{people_topic.name} with exercises to #{subject.name}"
+
+subject = Subject.create :name => 'Italian'
+number_topic = subject.topics.create :name => 'Numbers', :case_sensitive => false, :ignore_punctuation => true
+number_exercises = [
+  { 
+    :phrase => 'uno',
+    :response => 'one'
+  },
+  { 
+    :phrase => 'due',
+    :response => 'two'
+  },
+  { 
+    :phrase => 'tre',
+    :response => 'three'
+  },
+  { 
+    :phrase => 'quattro',
+    :response => 'four'
+  }
+]
+number_exercises.each do |ex|
+  number_topic.exercises.create :phrase => ex[:phrase], :response => ex[:response]
+end
+puts "added #{number_topic.name} with exercises to #{subject.name}"
+
+
