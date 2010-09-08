5816080d9d9a9bf5889379cc346f0bfacf171f75
Danny Sofer <danny@Works-iMac.local>
2010-09-08 15:02:28 +0100

    more cache tweaks

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,50 @@
-// cache testing: 1
+// cache testing: 6
+
+// Cache debugging. See http://jonathanstark.com/blog/2009/09/27/debugging-html-5-offline-application-cache/
+var cacheStatusValues = [];
+cacheStatusValues[0] = 'uncached';
+cacheStatusValues[1] = 'idle';
+cacheStatusValues[2] = 'checking';
+cacheStatusValues[3] = 'downloading';
+cacheStatusValues[4] = 'updateready';
+cacheStatusValues[5] = 'obsolete';
+
+var cache = window.applicationCache;
+cache.addEventListener('cached', logEvent, false);
+cache.addEventListener('checking', logEvent, false);
+cache.addEventListener('downloading', logEvent, false);
+cache.addEventListener('error', logEvent, false);
+cache.addEventListener('noupdate', logEvent, false);
+cache.addEventListener('obsolete', logEvent, false);
+cache.addEventListener('progress', logEvent, false);
+cache.addEventListener('updateready', logEvent, false);
+
+function logEvent(e) {
+    var online, status, type, message;
+    online = (navigator.onLine) ? 'yes' : 'no';
+    status = cacheStatusValues[cache.status];
+    type = e.type;
+    message = 'online: ' + online;
+    message+= ', event: ' + type;
+    message+= ', status: ' + status;
+    if (type == 'error' && navigator.onLine) {
+        message+= ' (probably a syntax error in manifest)';
+    }
+    console.log(message);
+}
+
+window.applicationCache.addEventListener(
+    'updateready',
+    function(){
+        window.applicationCache.swapCache();
+        console.log('swap cache has been called');
+    },
+    false
+);
+
+setInterval(function(){cache.update()}, 10000);
+//
+
 
 String.prototype.simplify = function () {
   return this.replace(/[,.!?\-\\']/g, '').toLowerCase();
