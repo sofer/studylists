77fa4086bd8b4cbaf217b83555f74017dc9b0200
D Sofer <d@sofer.com>
2010-09-23 08:39:50 +0100

    more sencha+icons

--- public/javascripts/sencha-app.js
+++ public/javascripts/sencha-app.js
@@ -1,3 +1,4 @@
+/*
 Ext.setup({
     tabletStartupScreen: 'tablet_startup.png',
     phoneStartupScreen: 'phone_startup.png',
@@ -14,7 +15,38 @@ Ext.setup({
             defaults: {
                 flex: 1
             },
-            items:
+            items: [{
+              ui: 'round',
+              text: 'nothing'
+            }]
+        });
+    }
+});
+*/
+
+Ext.setup({
+    icon: 'icon.png',
+    glossOnIcon: false,
+    tabletStartupScreen: 'tablet_startup.png',
+    phoneStartupScreen: 'phone_startup.png',
+    onReady: function() {
+        new Ext.TabPanel({
+            fullscreen: true,
+            type: 'dark',
+            sortable: true,
+            items: [{
+                title: 'Tab 1',
+                html: '1',
+                cls: 'card1'
+            }, {
+                title: 'Tab 2',
+                html: '2',
+                cls: 'card2'
+            }, {
+                title: 'Tab 3',
+                html: '3',
+                cls: 'card3'
+            }]
         });
     }
 });
