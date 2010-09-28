58f9dfd6113e3cd4d6a021996635f22a40662e81
D Sofer <d@sofer.com>
2010-09-28 18:17:36 +0100

    wip

---
+++ public/javascripts/data.js
@@ -0,0 +1,19 @@
+{
+    "text": "Subjects",
+    "items": [{
+        "text": "French",
+        "items": [{
+            "text": "Numbers",
+            "leaf": true
+          },{
+            "text": "People",
+            "leaf": true
+          }]
+        },{
+            "text": "Italian",
+            "items": [{
+                "text": "Numbers",
+                "leaf": true
+        }]
+   }]
+}
