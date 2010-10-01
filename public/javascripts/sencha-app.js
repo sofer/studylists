e6cb71c5fd1773deaccd14e775aaf6114d85a34e
D Sofer <d@sofer.com>
2010-10-01 09:21:38 +0100

    wip

--- public/javascripts/sencha-app.js
+++ public/javascripts/sencha-app.js
@@ -1,109 +1,65 @@
-// wrap in closure to avoid global vars.
-(function() {
-
 Ext.regModel('Subjects', {
     fields: [
         {name: 'text', type: 'string'}
     ]
 });
 
+Ext.setup({
+    icon: 'icon.png',
+    tabletStartupScreen: 'tablet_startup.png',
+    phoneStartupScreen: 'phone_startup.png',
+    glossOnIcon: false,
+    onReady: function(){
+      
+      var store = new Ext.data.TreeStore({
+          model: 'Subjects',
+          proxy: {
+              type: 'ajax',
+              url: 'javascripts/subjects.json', // check path
+              reader: {
+                  type: 'tree',
+                  root: 'items' // or children?
+              }
+          }
+      });
 
-var txtFld = new Ext.form.TextField({
-    label: 'Name'
-});
+        var nestedList = new Ext.NestedList({
+            fullscreen: true,
+            title: 'src/',
+            displayField: 'fileName',
+            // add a / for folder nodes in title/back button
+            getTitleTextTpl: function() {
+                return '{' + this.displayField + '}<tpl if="leaf !== true">/</tpl>';
+            },
+            // add a / for folder nodes in the list
+            getItemTextTpl: function() {
+                return '{' + this.displayField + '}<tpl if="leaf !== true">/</tpl>';
+            },
+            // provide a codebox for each source file
+            getDetailCard: function(record, parentRecord) {
+                return new Ext.ux.CodeBox({
+                    value: 'Loading...',
+                    scroll: 'both'
+                });
+            },
+            store: store
+        });
 
-var calculateDesiredWidth = function() {
-    var viewWidth = Ext.Element.getViewportWidth(),
-        desiredWidth = Math.min(viewWidth, 400) - 10;
 
-    return desiredWidth;
-};
+        nestedList.on('leafitemtap', function(subList, subIdx, el, e, detailCard) {
+            var ds = subList.getStore(),
+                r  = ds.getAt(subIdx);
 
-
-var editPnl = new Ext.Panel({
-    floating: true,
-    centered: true,
-    modal: true,
-    width: calculateDesiredWidth(),
-    height: 170,
-
-    dockedItems: [{
-        dock: 'top',
-        xtype: 'toolbar',
-        title: 'Editing Item'
-    },{
-        dock: 'bottom',
-        xtype: 'toolbar',
-        items: [{
-            text: 'Cancel',
-            handler: function() {
-                editPnl.hide();
-            }
-        },{
-            xtype: 'spacer'
-        },{
-            text: 'Change',
-            handler: function() {
-                var activeList = demos.NestedList.getActiveItem(),
-                    record     = activeList.getSelectedRecords()[0];
-                record.set('text', txtFld.getValue());
-
-                // Workaround: selection *should* be maintained.
-                activeList.select(record);
-
-                editPnl.hide();
-            }
-        }]
-    }],
-    items: [{
-        xtype: 'fieldset',
-        items: [txtFld]
-    }]
-});
-
-Ext.EventManager.onOrientationChange(function() {
-    editPnl.setWidth(calculateDesiredWidth());
-});
-
-var editBtn = new Ext.Button({
-    text: 'Edit',
-    disabled: true,
-    handler: function() {
-        editPnl.show();
-
-        var activeList = demos.NestedList.getActiveItem(),
-            record     = activeList.getSelectedRecords()[0];
-
-        txtFld.setValue(record.get('text'));
+            Ext.Ajax.request({
+                url: '../../src/' + r.get('id'),
+                success: function(response) {
+                    detailCard.setValue(response.responseText);
+                },
+                failure: function() {
+                    detailCard.setValue("Loading failed.");
+                }
+            });
+        });
     }
 });
 
-
-var store = new Ext.data.TreeStore({
-    model: 'Subjects',
-    proxy: {
-        type: 'ajax',
-        url: 'subjects.json', // check path
-        reader: {
-            type: 'tree',
-            root: 'items'
-        }
-    }
-});
-
-demos.NestedList = new Ext.NestedList({
-    plugins: [new Ext.LeafSelectedPlugin()],
-    toolbar: {
-        items: [{xtype: 'spacer'}, editBtn]
-    },
-    store: store
-});
-
-// leafselected event is provided by the LeafSelectedPlugin.js
-demos.NestedList.on('leafselected', function(enabled) {
-    editBtn.setDisabled(!enabled);
-});
-
-
-
-})();
