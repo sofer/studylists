4f2135c1daa97303ea36c49496016b3651b57c23
D Sofer <d@sofer.com>
2010-10-08 09:12:05 +0100

    wip

--- public/javascripts/sencha-app.js
+++ public/javascripts/sencha-app.js
@@ -1,33 +1,189 @@
 Ext.setup({
-    icon: 'icon.png',
-    glossOnIcon: false,
     tabletStartupScreen: 'tablet_startup.png',
     phoneStartupScreen: 'phone_startup.png',
-    onReady: function() {
+    icon: 'icon.png',
+    glossOnIcon: false,
+    onReady : function() {
+        Ext.regModel('Contact', {
+            fields: ['firstName', 'lastName']
+        });
+
+        var groupingBase = {
+            tpl: '<tpl for="."><div class="contact"><strong>{firstName}</strong> {lastName}</div></tpl>',
+            itemSelector: 'div.contact',
+
+            singleSelect: true,
+            grouped: true,
+            indexBar: true,
+
+            disclosure: {
+                scope: 'test',
+                handler: function(record, btn, index) {
+                    alert('Disclose more info for ' + record.get('firstName'));
+                }
+            },
+
+            store: new Ext.data.Store({
+                model: 'Contact',
+                sorters: 'firstName',
+
+                getGroupString : function(record) {
+                    return record.get('firstName')[0];
+                },
+
+                data: [
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Ape', lastName: 'Evilias'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Ape', lastName: 'Evilias'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Ape', lastName: 'Evilias'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Ape', lastName: 'Evilias'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Tommy', lastName: 'Maintz'},
+                    {firstName: 'Ed', lastName: 'Spencer'},
+                    {firstName: 'Jamie', lastName: 'Avins'},
+                    {firstName: 'Aaron', lastName: 'Conran'},
+                    {firstName: 'Dave', lastName: 'Kaneda'},
+                    {firstName: 'Michael', lastName: 'Mullany'},
+                    {firstName: 'Abraham', lastName: 'Elias'},
+                    {firstName: 'Jay', lastName: 'Robinson'},
+                    {firstName: 'Zed', lastName: 'Zacharias'}
+                ]
+            })
+        };
 
 
-      var nestedList = new Ext.NestedList({
-          fullscreen: true,
-          title: 'src/',
-          displayField: 'fileName',
-          // add a / for folder nodes in title/back button
-          getTitleTextTpl: function() {
-              return '{' + this.displayField + '}<tpl if="leaf !== true">/</tpl>';
-          },
-          // add a / for folder nodes in the list
-          getItemTextTpl: function() {
-              return '{' + this.displayField + '}<tpl if="leaf !== true">/</tpl>';
-          },
-          // provide a codebox for each source file
-          getDetailCard: function(record, parentRecord) {
-              return new Ext.ux.CodeBox({
-                  value: 'Loading...',
-                  scroll: 'both'
-              });
-          },
-          //store: store
-      });
+        if (!Ext.is.Phone) {
+            new Ext.List(Ext.apply(groupingBase, {
+                floating: true,
+                width: 350,
+                height: 370,
+                centered: true,
+                modal: true,
+                hideOnMaskTap: false
+            })).show();
+        }
+        else {
+            new Ext.List(Ext.apply(groupingBase, {
+                fullscreen: true
+            }));
+        }
     }
-
 });
-
